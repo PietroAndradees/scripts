8a5658c4ebab57d668341228c9718d4523090439
PietroAndradees <pietroandradees@gmail.com>
2023-05-02 17:05:20 -0300

    v1

--- conecta_ec2_V2.sh
+++ conecta_ec2_V2.sh
@@ -4,6 +4,7 @@
 Color_Off='\033[0m'       # Text Reset
 # Regular Colors
 Blue='\033[0;34m'         # Blue
+Red='\033[0;31m'          # Red
 # Bold
 BYellow='\033[1;33m'      # Yellow
 
@@ -12,17 +13,24 @@ touch list
 echo "Instâncias previamente encontradas:"
 echo ""
 cat list | sort | uniq -c | sort -nr | awk '{print $2, $3}' | head -n 20 | awk '{print NR,"=>",$0}'
-echo -e "${Blue}0 => Para buscar uma instância no console EC2 ☁️${Color_Off}"
+echo -e "${Blue}0 => Para buscar uma instância no console EC2 ☁️ ${Color_Off}"
 echo ""
-echo -e "${BYellow} --->   Para fechar a aplicação pressione CRTL+C <--- ${Color_Off}"
+echo -e "${Red}C => Para limpar o histórico de buscar EC2 🚫 ${Color_Off}"
+echo ""
+echo -e "\033[5m ${BYellow} --->   Para fechar a aplicação pressione CRTL+C <--- ${Color_Off}"
 echo ""
 read -p "Selecione o número de uma instância para iniciar uma sessão do SSM: " selection_history
 
-if [ $selection_history -eq 0 ]
+if [ "$selection_history" == 'c' ] || [ "$selection_history" == 'C' ]
+then
+echo -n "" > list
+echo " Histórico limpo com sucesso !!! "
+clear
+elif [ $selection_history -eq 0 ]
 then
 clear
 echo ""
-echo -e "${BYellow} --->   Para fechar a aplicação pressione CRTL+C <--- ${Color_Off}"
+echo -e "\033[5m ${BYellow} --->   Para fechar a aplicação pressione CRTL+C <--- ${Color_Off}"
 echo ""
 read -p "Digite o nome da instância EC2 que deseja procurar 🖳: " instance_name
 instances=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=*$instance_name*" --query "Reservations[*].Instances[*].[InstanceId, Tags[?Key=='Name'].Value | [0]]" --output text | head -n 20)
@@ -36,7 +44,7 @@ echo "Instâncias encontradas:"
 echo ""
 echo "$instances" | awk '{print NR,"=>",$0}'
 echo ""
-echo -e "${BYellow} --->   Para fechar a aplicação pressione CRTL+C <--- ${Color_Off}"
+echo -e "\033[5m ${BYellow} --->   Para fechar a aplicação pressione CRTL+C <--- ${Color_Off}"
 echo ""
 read -p "Selecione o número da instância para iniciar uma sessão do SSM 🖧: " selection
 
