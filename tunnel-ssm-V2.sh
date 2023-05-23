95185b5cf957371a70d2bed50b0b6b560f200a39
PietroAndradees <pietroandradees@gmail.com>
2023-05-23 13:24:38 -0300

    tunnel-ssm-v2

--- tunnel-ssm-V2.sh
+++ tunnel-ssm-V2.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 #REGION=us-east-1
+pkill -9 sshuttle
 REGION=$(aws configure get region)
-CIDR="10.64.0.0/16 192.168.0.0/16"
-BASTION_INSTANCE="i-05360df5b9a5eb8ef" #34.226.74.203
-TRANSPARENT_PROXY="0.0.0.0"
+CIDR="10.64.0.0/16 192.168.240.0/21"
+REDE_LOCAL="192.168.100.0/24"
+BASTION_INSTANCE=i-05360df5b9a5eb8ef #34.226.74.203
+NOME=ec2-user
+TRANSPARENT_PROXY="0.0.0.0:0"
+DOCKER_GW="0.0.0.0"
+REDE_DOCKER="172.17.0.0/24"
 echo "Conectando-se a VPC de região "$REGION
-echo "Adicionando a rota "$CIDR
+echo "Adicionando a rota $CIDR para VPC e para o Docker $REDE_DOCKER" 
 echo "Usando a instancia $BASTION_INSTANCE como Tunnel"
 echo ""
 echo "Para fechar o Tunnel pressione Ctrl+c"
 echo "" 
-sshuttle -l $TRANSPARENT_PROXY --dns -N -r $NOME@$BASTION_INSTANCE $CIDR
-#sshuttle -l $TRANSPARENT_PROXY --dns -N -r $NOME@$BASTION_INSTANCE $CIDR --ssh-cmd 'ssh -i ~/.ssh/\$NOME'
+sshuttle --dns -N -r $NOME@$BASTION_INSTANCE $CIDR &
+sleep 0.5
+sshuttle -l $DOCKER_GW -r $NOME@$BASTION_INSTANCE $CIDR -x $REDE_DOCKER &
+#sshuttle -l $TRANSPARENT_PROXY --disable-ipv6 --dns -N -r $NOME@$BASTION_INSTANCE $CIDR --ssh-cmd 'ssh -i ~/.ssh/\$NOME'
+#sshuttle --dns -N -r ec2-user@i-05360df5b9a5eb8ef 10.64.0.0/16 10.32.0.0/16
