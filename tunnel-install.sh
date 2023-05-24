f4456195fe7c2094c8032704a06b5c2c1c6f502f
PietroAndradees <pietroandradees@gmail.com>
2023-05-24 15:30:49 -0300

    v1.1

--- tunnel-install.sh
+++ tunnel-install.sh
@@ -11,6 +11,10 @@ read -p "Qual gerenciador de pacotes você está usando? (apt/yum): " resposta
 if [ "$resposta" = "apt" ]; then
   curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
   sudo dpkg -i session-manager-plugin.deb
+  touch ~/.ssh/config
+  echo "# SSH over Session Manager
+host i-* mi-*
+    ProxyCommand sh -c "aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'"" > ~/.ssh/config
   sudo apt-get install sshuttle -y
   cd /usr/local/bin
   sudo wget https://raw.githubusercontent.com/PietroAndradees/scripts/main/tunnel-ssm-V2.sh -O tunnel-aws
@@ -18,6 +22,10 @@ if [ "$resposta" = "apt" ]; then
   sudo nano tunnel-aws
 elif [ "$resposta" = "yum" ]; then
   sudo yum install -y https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm
+  touch ~/.ssh/config
+  echo "# SSH over Session Manager
+host i-* mi-*
+    ProxyCommand sh -c "aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'"" > ~/.ssh/config
   sudo yum install -y sshuttle
   cd /usr/local/bin
   sudo wget https://raw.githubusercontent.com/PietroAndradees/scripts/main/tunnel-ssm-V2.sh -O tunnel-aws
