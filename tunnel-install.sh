0ad30588f552b5e0d32e9459592a5d4849bb1bb0
PietroAndradees <pietroandradees@gmail.com>
2023-05-24 15:21:30 -0300

    tunnel-install-v1

--- tunnel-install.sh
+++ tunnel-install.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+sleep 2
+
+read -p "Qual gerenciador de pacotes você está usando? (apt/yum): " resposta
+
+if [ "$resposta" = "apt" ]; then
+  curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+  sudo dpkg -i session-manager-plugin.deb
+  sudo apt-get install sshuttle -y
+  cd /usr/local/bin
+  sudo wget https://raw.githubusercontent.com/PietroAndradees/scripts/main/tunnel-ssm-V2.sh -O tunnel-aws
+  sudo chmod +x tunnel-aws
+  sudo nano tunnel-aws
+elif [ "$resposta" = "yum" ]; then
+  sudo yum install -y https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm
+  sudo yum install -y sshuttle
+  cd /usr/local/bin
+  sudo wget https://raw.githubusercontent.com/PietroAndradees/scripts/main/tunnel-ssm-V2.sh -O tunnel-aws
+  sudo chmod +x tunnel-aws
+  sudo nano tunnel-aws
+else
+  echo "Resposta inválida. Por favor, insira 'apt' ou 'yum'."
+fi
+
+echo "Script finalizado. Para executá-lo, execute o comando 'tunnel-aws'."
+echo "Não se esqueça de configurar o arquivo 'tunnel-aws' para o seu usuário e de rodar o comando "aws configure sso" caso nunca tenha utilizado o AWS CLI"
--- tunnel-ssm-V2.sh
+++ tunnel-ssm-V2.sh
@@ -3,9 +3,9 @@
 pkill -9 sshuttle
 REGION=$(aws configure get region)
 CIDR="10.64.0.0/16 192.168.240.0/21"
-REDE_LOCAL="192.168.100.0/24"
+REDE_LOCAL="XXX.XXX.XXX.XXX/XX" #SUA REDE LOCAL
 BASTION_INSTANCE=i-05360df5b9a5eb8ef #34.226.74.203
-NOME="pietro.andrade"
+NOME="NOME.SOBRENOME" #O mesmo prefixo do email coorporativo
 TRANSPARENT_PROXY="0.0.0.0:0"
 DOCKER_GW="0.0.0.0"
 REDE_DOCKER="172.17.0.0/24"
@@ -18,5 +18,6 @@ echo ""
 sshuttle --dns -N -r $NOME@$BASTION_INSTANCE $CIDR &
 sleep 0.5
 sshuttle -l $DOCKER_GW -r $NOME@$BASTION_INSTANCE $CIDR -x $REDE_DOCKER &
+
 #sshuttle -l $TRANSPARENT_PROXY --disable-ipv6 --dns -N -r $NOME@$BASTION_INSTANCE $CIDR --ssh-cmd 'ssh -i ~/.ssh/\$NOME'
 #sshuttle --dns -N -r ec2-user@i-05360df5b9a5eb8ef 10.64.0.0/16 10.32.0.0/16
